fff781530a15309341cecc377828a8d00372ec3e
JoFrhwld <JoFrhwld@gmail.com>
2023-03-15 11:39:28 -0400

    re-add nested indexing

--- src/aligned_textgrid/sequences/tiers.py
+++ src/aligned_textgrid/sequences/tiers.py
@@ -262,8 +262,19 @@ class RelatedTiers:
     def __contains__(self, item):
         return item in self.tier_list
     
-    def __getitem__(self, idx):
-        return self.tier_list[idx]
+    def __getitem__(
+            self, 
+            idx: int | list
+            ):
+        if type(idx) is int:
+            return self.tier_list[idx]
+        if len(idx) != len(self):
+            raise Exception("Attempt to index with incompatible list")
+        if type(idx) is list:
+            out_list = []
+            for x, tier in zip(idx, self.tier_list):
+                out_list.append(tier[x])
+            return(out_list)        
         
     def __iter__(self):
         self._idx = 0
