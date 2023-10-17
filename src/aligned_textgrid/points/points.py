0b40aa085586407eb7bb406d6334a232dab9ade8
JoFrhwld <JoFrhwld@gmail.com>
2023-10-17 16:47:04 -0400

    fleshing out points class

--- src/aligned_textgrid/points/points.py
+++ src/aligned_textgrid/points/points.py
@@ -1,71 +1,14 @@
 from praatio.utilities.constants import Point
+from aligned_textgrid.mixins.mixins import PrecedenceMixins, TierMixins
+from aligned_textgrid.sequences.tiers import SequenceTier
+import warnings
 
-class PrecedenceMixins:
-
-    def set_fol(
-            self, next_int):
-        """_Sets the following instance_
-
-        Args:
-            next_int (SequenceInterval): 
-                Sets the `next_int` as the `fol` interval.
-                Must be of the same class as the current object.
-                That is, `type(next_int) is type(self)`
-        """
-        if next_int is self:
-            raise Exception(f"A segment can't follow itself.")
-        if self.label == "#":
-            return
-        if self.fol is next_int:
-            return
-        elif type(next_int) is type(self):
-            self.fol = next_int
-            self.fol.set_prev(self)
-        else:
-            raise Exception(f"Following segment must be an instance of {type(self).__name__}")
-
-    def set_prev(self, prev_int):
-        """_Sets the previous intance_
-
-        Args:
-            prev_int (SequenceInterval):
-                Sets the `prev_int` as the `prev` interval
-                Must be of the same class as the current object.
-                That is, `type(prev_int) is type(self)`                
-        """
-        if prev_int is self:
-            raise Exception("A segment can't precede itself.")
-        if self.label == "#":
-            return
-        if self.prev is prev_int:
-            return
-        elif type(prev_int) is type(self):
-            self.prev = prev_int
-            self.prev.set_fol(self)
-        else:
-            raise Exception(f"Previous segment must be an instance of {type(self).__name__}")
-    
-    def set_final(self):
-        """_Sets the current object as having no `fol` interval_
-        
-        While `self.fol` is defined for these intervals, the actual
-        instance does not appear in `self.super_instance.subset_list`
-        """
-        self.set_fol(type(self)(Interval(None, None, "#")))  
-
-    def set_initial(self):
-        """_Sets the current object as having no `prev` interval_
-
-        While `self.prev` is defined for these intervals, the actual 
-        instance does not appear in `self.super_instance.subset_list`
-        """
-        self.set_prev(type(self)(Interval(None, None, "#")))
-
-class  SeqPoint:
+class  SequencePoint(PrecedenceMixins, TierMixins):
     def __init__(
             self,
             Point
         ):
+        super().__init__()
         if not Point:
             Point = Point(0, 0)
         
@@ -75,12 +18,34 @@ class  SeqPoint:
         self.intier = None
         self.tiername = None
         self.pointspool = None
+        self.fol = None
+        self.prev = None
+        self.reference_tier = None
+
+    def __repr__(self) -> str:
+        out_string = f"Class {type(self).__name__}, label: {self.label}"
+        return out_string
+    
+    def __contains__(self, item):
+        warnings.warn("`in` is not a valid operator for a SequncePoint")
+        return None
+    
+    def __getitem__(self, idex):
+        warnings.warn("Indexing is not valid for a SequencePoint")
+        return None
 
     def set_intier(self, tier):
         self.intier = tier
     
     def set_tiername(self, tier):
         self.tiername = tier.name
+
+    # def get_interval_index_at_time(self, tier = None):
+    #     if tier and isinstance(tier, SequenceTier)
+    #         int_idx = tier.get_interal_at_time(self.time)
+    #         return int_idx
+    #
+    # def get_interval_at_point(self, tier=None):
 
     def set_pointspool(self):
         pass
