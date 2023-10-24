22b4c1e1bab174546f4a3e0ccb42438238a2af87
JoFrhwld <JoFrhwld@gmail.com>
2023-10-24 12:59:54 -0400

    Typing and docs for points

--- src/aligned_textgrid/points/points.py
+++ src/aligned_textgrid/points/points.py
@@ -2,13 +2,44 @@ from praatio.utilities.constants import Point
 from aligned_textgrid.mixins.mixins import PrecedenceMixins, TierMixins
 from aligned_textgrid.sequences.tiers import SequenceTier
 from aligned_textgrid.sequences.sequences import SequenceInterval
+from typing import Union
+from typing_extensions import Self
 import warnings
 import numpy as np
 
 class  SequencePoint(PrecedenceMixins, TierMixins):
+    """_A point class_
+    
+    Args:
+        point (Point): _a `praatio` point object_
+    
+    Attributes:
+        ...:
+            All attributes and methods included in PrecedenceMixins and TierMixins
+        time (float):
+          Time value associated with the point.
+        label (str):
+          Label associated with the point
+        intier (SequencePointTier):
+          If the SequencePoint is within a tier, this accesses the tier.
+        fol (SequencePoint):
+          If defined, the following SequencePoint within the same tier
+        prev (SequencePoint):
+          If defined, the previous SequencePoint within the same tier.
+        fol_distance (float):
+          If `fol` is defined, the difference between the current point and `fol`
+          (should be >= 0)
+        prev_distance (float):
+          if `prev` is defined, the difference between the current point and `prev`
+          (should be <= 0).
+
+        
+
+    """
+
     def __init__(
             self,
-            point = None
+            point = Point(0, "")
         ):
         super().__init__()
         if not point:
@@ -72,7 +103,22 @@ class  SequencePoint(PrecedenceMixins, TierMixins):
             return None        
 
     ## methods
-    def distance_from(self, entry):
+    def distance_from(
+            self, 
+            entry: Self|SequenceInterval
+        ) -> Union[float,np.array]:
+        """_distance from an entry_
+
+        Args:
+            entry (Self | SequenceInterval):
+                A point or an interval to get the distance of the current 
+                point from
+
+        Returns:
+            (Union[float,np.array]):
+              a single value in the case of a point, a numpy array in
+              the case of an interval.
+        """
         if isinstance(entry, SequencePoint):
             return self.time - entry.time
         
@@ -80,12 +126,37 @@ class  SequencePoint(PrecedenceMixins, TierMixins):
             entry_times = np.array([entry.start, entry.end])
             return self.time - entry_times
 
-    def get_interval_index_at_time(self, tier = None):
+    def get_interval_index_at_time(
+            self, 
+            tier: SequenceTier = None
+        ) -> int:
+        """_Get the index of an interval at the point's time_
+
+        Args:
+            tier (SequenceTier): A SequenceTier.
+
+        Returns:
+            (int): 
+              The index of the SequenceInterval within which the point falls
+        """
         if tier and isinstance(tier, SequenceTier):
             int_idx = tier.get_interval_at_time(self.time)
             return int_idx
     
-    def get_interval_at_point(self, tier=None):
+    def get_interval_at_point(
+            self, 
+            tier: SequenceTier=None
+        ) -> SequenceInterval:
+        """_Get the SequenceInterval the current point falls within_
+
+        Args:
+            tier (SequenceTier):
+                The sequence tier within which to look for a matching SequenceInterval
+
+        Returns:
+            (SequenceInterval): 
+              The SequenceInterval within which the current point falls
+        """
         if tier and isinstance(tier, SequenceTier):
             return tier[self.get_interval_index_at_time(tier)]
 
