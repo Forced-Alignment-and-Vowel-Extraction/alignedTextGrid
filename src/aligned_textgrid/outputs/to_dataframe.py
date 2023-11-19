55680e9e38843a495a38f1301d7c3bb28ff92b91
JoFrhwld <JoFrhwld@gmail.com>
2023-11-19 10:19:09 -0500

    dataframe outputs

--- src/aligned_textgrid/outputs/to_dataframe.py
+++ src/aligned_textgrid/outputs/to_dataframe.py
@@ -1,36 +1,113 @@
 import polars as pl
+from aligned_textgrid import SequenceInterval, \
+                             SequencePoint, \
+                             SequenceTier, \
+                             SequencePointTier, \
+                             AlignedTextGrid
 
-def interval_to_df(self):
-    attributes_to_get =[
-        "id"
+from aligned_textgrid.sequences.tiers import TierGroup
+from aligned_textgrid.points.tiers import PointsGroup
+
+from typing import Union
+
+def sequence_to_df(
+        obj: SequenceInterval | SequencePoint
+        ) -> pl.DataFrame:
+    
+    attributes_to_get = [
+        "id",
         "tier_index",
-        "start",
-        "end",
         "label"
     ]
-    class_name = type(self).__name__
+
+    if isinstance(obj, SequenceInterval):
+        attributes_to_get += ["start", "end"]
+
+    if isinstance(obj, SequencePoint):
+        attributes_to_get += ["time"]
+
+    class_name = type(obj).__name__
     col_names = [
         f"{class_name}_{att}" for att in attributes_to_get
-    ]    
+    ]
 
     sub_df_rows = None
-    if len(self.contains) > 0:
-        sub_df_list = [interval_to_df(x) for x in self.contains]
-        sub_df = pl.concat(sub_df_list, how="vertical")
+    if len(obj.contains) > 0:
+        sub_df_list = [sequence_to_df(x) for x in obj.contains]
+        sub_df = pl.concat(sub_df_list, how="diagonal")
         sub_df_rows = sub_df.shape[0]
 
     out_dict = {
-        att: getattr(self, att)
+        att: getattr(obj, att)
         for att in attributes_to_get
     }
 
     df = pl.DataFrame(out_dict, schema=col_names)
     if sub_df_rows:
         df = df.with_columns(
-            n = pl.lit(sub_df_rows)
+            n=pl.lit(sub_df_rows)
         ).select(
             pl.exclude("n").repeat_by("n").explode()
         )
-        df = pl.concat(df, sub_df, how="horizontal")
+        df = pl.concat([df, sub_df], how="horizontal")
 
-    return(df)
+    return df
+
+
+def tier_to_df(
+        obj: SequenceInterval | SequencePointTier
+        ) -> pl.DataFrame:
+    
+    all_interval_dfs = [
+        sequence_to_df(x) for x in obj
+    ]
+
+    out_df = pl.concat(all_interval_dfs, how="diagonal")
+    return out_df
+
+
+def tiergroup_to_df(
+        obj: TierGroup | PointsGroup
+        ) -> pl.DataFrame:
+
+    out_df = tier_to_df(obj[0])
+    if hasattr(obj, "name"):
+        out_df.with_columns(
+            name=pl.lit(obj.name)
+        )
+
+    return out_df
+
+
+def textgrid_to_df(
+        obj: AlignedTextGrid
+        ) -> pl.DataFrame:
+
+    all_df = [
+        tiergroup_to_df(x) for x in obj
+    ]
+
+    out_df = pl.concat(all_df, how="diagonal")
+
+    return out_df
+
+
+def to_df(
+        obj: SequenceInterval | SequencePoint | SequenceTier |
+        SequencePointTier | TierGroup | PointsGroup |
+        AlignedTextGrid
+        ) -> pl.DataFrame:
+    
+    if isinstance(obj, SequenceInterval) or isinstance(obj, SequencePoint):
+       return sequence_to_df(obj)
+    
+    if isinstance(obj, SequenceTier) or isinstance(obj, SequencePointTier):
+        return tier_to_df(obj)
+    
+    if isinstance(obj, TierGroup) or isinstance(obj, PointsGroup):
+        return tiergroup_to_df(obj)
+
+    if isinstance(obj, AlignedTextGrid):
+        return textgrid_to_df(obj)
+
+    raise ValueError("obj is not an aligned-textgrid class.")
