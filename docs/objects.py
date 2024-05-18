0571e5eec2aa6658c289e09c4a1f22433f7a6362
JoFrhwld <JoFrhwld@gmail.com>
2024-05-18 10:54:45 -0400

    add exported classes to sphinx inventory

--- docs/objects.py
+++ docs/objects.py
@@ -1,6 +1,17 @@
+import aligned_textgrid
 import sphobjinv as soi
 from pathlib import Path
 inv = soi.Inventory(fname_plain = Path("objects.txt"))
+
+for idx in range(len(inv.objects)):
+    obj = inv.objects[idx]
+    basename = obj.name.split(".")[-1]
+    if hasattr(aligned_textgrid, basename):
+        obj_dict = obj.json_dict()
+        obj_dict["name"] = "aligned_textgrid." + basename
+        new_obj = soi.DataObjStr(**obj_dict)
+        inv.objects.append(new_obj)
+
 df = inv.data_file()
 dfc = soi.compress(df)
 soi.writebytes("objects.inv", dfc)
