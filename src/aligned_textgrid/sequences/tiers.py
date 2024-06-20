0fd98edafc9afdcb376467e3d6d65197c258102e
JoFrhwld <JoFrhwld@gmail.com>
2024-06-20 18:38:52 -0400

    tiergroup cleanup

--- src/aligned_textgrid/sequences/tiers.py
+++ src/aligned_textgrid/sequences/tiers.py
@@ -466,37 +466,21 @@ class TierGroup(Sequence,TierGroupMixins, WithinMixins):
         up_tier.append(new_interval)
         
     def cleanup(self):
+        for idx, tier in enumerate(self):
+            if issubclass(tier.subset_class, Bottom):
+                break
+
+            for interval in tier:
+                interval.cleanup()
+        
+        for idx, tier in enumerate(reversed(self)):
+            for interval in tier:
+                self.project_up(interval)
+
         for tier in self:
             tier.cleanup()
         
-   
-
-        starts = np.array([tier.xmin for tier in self])
-        ends = np.array([tier.xmax for tier in self])
-
-        if ~np.allclose(starts.ptp(), 0.0):
-            new_start = self.xmin
-            
-            for tier in self:
-                if np.allclose(tier.xmin, new_start):
-                    continue
-
-                empty_interval = tier.entry_class((new_start, tier.xmin, ""))
-                tier.append(empty_interval)
-            
-
-        
-        if ~np.allclose(ends.ptp(), 0.0):
-            new_end = self.xmax
-
-            for tier in self:
-                if np.allclose(tier.xmax, new_end):
-                    continue
-                
-                empty_interval = tier.entry_class((tier.xmax, new_end, ""))
-                tier.append(empty_interval)
-
-        self = TierGroup(self)
+        self.re_relate()
 
 
     def shift(
